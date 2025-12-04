ca3122f6cba35e6e93bfeb3688322992f9412f3c
Peder Aa. Hoff <peder.aa.hoff@gmail.com>
2025-12-04 19:39:00 +0100

    Fullførte task2

--- 2025/3. desember/task2.py
+++ 2025/3. desember/task2.py
@@ -1,44 +1,23 @@
-def counting_sort(iter_list):
-    count = [0]*10
-    memory = [[]]*10
+def find_largest_left(id, n):
+    print(f"id: {id}")
+    maxid = 0
+    for i in range(len(id)-n):
+        if id[i] > id[maxid]:
+            maxid = i
+        
+    if n == 0:
+        return str(id[maxid])
 
-    for i in range(5):
-        print(memory[int(iter_list[i])])
-        print(int(iter_list[i]))
-        print()
-
-        count[int(iter_list[i])] += 1
-        memory[int(iter_list[i])].append(i)
-
-  
-    l = 0
-    k = 0
-    max_batt = 12
-    ku = []*len(iter_list)
-    index = 9
-
-    c = 0
-    while l < max_batt and c < 10000:
-        c += 1
-        try:
-            ku[memory[index][k]] = str(index)
-            k += 1
-            l += 1
-
-        except:
-            index -= 1
-            k = 0
-    
-    return int("".join(ku))
-
+    return str(id[maxid]) + find_largest_left(id[maxid+1:], n-1)
 
 with open("2025\\3. desember\\input.txt") as file:
     total = 0
-
     for line in file:
         if line[-1] == '\n':
             line = line[:-1]
         
-        total += counting_sort(line)
+        l = int(find_largest_left(line, 12-1))
+        print(f"subsum: {l}")
+        total += l
 
     print(f"Svar: {total}")
