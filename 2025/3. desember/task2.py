915990425f973eb6531774d5cdc44205cb27f9b4
Peder Aa. Hoff <peder.aa.hoff@gmail.com>
2025-12-04 18:29:09 +0100

    Oppg 2

--- 2025/3. desember/task2.py
+++ 2025/3. desember/task2.py
@@ -1,6 +1,36 @@
+def counting_sort(iter_list):
+    count = [0]*10
+    memory = [[]]*10
 
-def counting_sort(list): # TODO: Implement counting sort
-    pass
+    for i in range(5):
+        print(memory[int(iter_list[i])])
+        print(int(iter_list[i]))
+        print()
+
+        count[int(iter_list[i])] += 1
+        memory[int(iter_list[i])].append(i)
+
+  
+    l = 0
+    k = 0
+    max_batt = 12
+    ku = []*len(iter_list)
+    index = 9
+
+    c = 0
+    while l < max_batt and c < 10000:
+        c += 1
+        try:
+            ku[memory[index][k]] = str(index)
+            k += 1
+            l += 1
+
+        except:
+            index -= 1
+            k = 0
+    
+    return int("".join(ku))
+
 
 with open("2025\\3. desember\\input.txt") as file:
     total = 0
@@ -8,8 +38,7 @@ with open("2025\\3. desember\\input.txt") as file:
     for line in file:
         if line[-1] == '\n':
             line = line[:-1]
-
-        new_line = counting_sort(line)
-        total += int("".join(new_line[0:12]))
+        
+        total += counting_sort(line)
 
     print(f"Svar: {total}")
